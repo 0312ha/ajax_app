88c654be737b1d43923a49338dee1d006269ec2a
0312ha <71496299+0312ha@users.noreply.github.com>
2020-10-01 11:02:05 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
+    @posts = Post.all.order(id: "DESC")
   end
 end
